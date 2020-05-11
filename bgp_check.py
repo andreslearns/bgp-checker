d2b4faf7e9fe2beb048f33a76858759894c64a5b
Andrew Soltes <soltesandrew@gmail.com>
2020-05-11 10:57:03 -0400

    added p2p features and source when pinging the neighbors

--- bgp_check.py
+++ bgp_check.py
@@ -1,8 +1,10 @@
+
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
 # from nornir.plugins.functions.text import print_result
 from tabulate import tabulate
-from colorama import Fore
+from colorama import Fore, Style
+
 
 # empty list for appending
 table = []
@@ -15,8 +17,9 @@ headers = [
     "Prefix",
     "Uptime",
     "BGP Status",
+    "Ping P2P Result",
+    "Ping P2P Summary"
 ]
-
 
 def bgp_check(task):
     """will execute command: show ip bgp in all hosts included in hosts.yaml"""
@@ -27,6 +30,7 @@ def bgp_check(task):
     bgp_sums = task.host["bgp_sum"]
     ipaddress = task.host.hostname
     # bgp states for "up" or "down" status
+
     bgp_down_states = [
         "Idle (Admin)",
         "Idle (PfxCt)",
@@ -52,27 +56,53 @@ def bgp_check(task):
         # R2 assigning description base on bgp neighbors
         if neighbor == "120.89.30.27":
             desc = "EASTERN BACKUP"
+            source = "180.232.122.14"
         elif neighbor == "120.89.30.28":
             desc = "EASTERN MAIN"
+            source = " 180.232.122.14"
         elif neighbor == "172.17.32.165":
             desc = "INCAPSULA HONGKONG"
+            neighbor ="107.154.26.52"
+            source = "180.232.122.14"
         elif neighbor == "172.17.160.13":
             desc = "INCAPSULA OSAKA"
+            neighbor = "107.154.33.5"
+            source = "180.232.122.14"
         elif neighbor == "192.168.88.1":
             desc = "iBGP CORE-R1"
+            source = "192.168.88.2"
         # R1 assigning description base on bgp neighbors
         elif neighbor == "121.58.215.185":
             desc = "CONVERGE"
+            source = "121.58.215.186"
         elif neighbor == "203.177.110.117":
             desc = "GLOBE"
+            source = "203.177.110.118"
         elif neighbor == "172.17.32.161":
             desc = "INCAPSULA HONGKONG"
+            neighbor = "107.154.26.52"
+            source = "121.58.215.186"
         elif neighbor == "172.17.160.9":
             desc = "INCAPSULA OSAKA"
+            neighbor = "107.154.33.5"
+            source = "121.58.215.186"
         elif neighbor == "192.168.88.2":
             desc = "iBGP CORE-R2"
+            source = "192.168.88.1"
         else:
-            desc = "BGP ROUTER"
+            desc = "BGP NEIGHBOR ROUTER"
+
+
+        ping_cmd =  task.run(netmiko_send_command, command_string = f"ping {neighbor} repeat 20 source {source}")
+        task.host["ping"] = ping_cmd.result
+        ping_results = task.host["ping"]
+        ping_results = ping_results.split()
+        ping = ping_results[24]
+
+        if not "!!!!!!!!!!!!!!!!!!!!" in ping:
+            ping_sum = Fore.RED + Style.BRIGHT + "INTERMITTENT" + Fore.RESET
+        else:
+            ping_sum = Fore.GREEN + Style.BRIGHT + "OK" + Fore.RESET
 
         if prefix not in bgp_down_states:
             table.append(
@@ -84,7 +114,10 @@ def bgp_check(task):
                     neigh_as,
                     prefix,
                     up_down,
-                    Fore.GREEN + ("UP") + Fore.RESET,
+                    Fore.GREEN + Style.BRIGHT + ("UP") + Fore.RESET,
+                    ping,
+                    ping_sum,
+
                 ]
             )
         elif prefix in bgp_down_states:
@@ -97,7 +130,9 @@ def bgp_check(task):
                     neigh_as,
                     prefix,
                     up_down,
-                    Fore.RED + ("DOWN") + Fore.RESET,
+                    Fore.RED + Style.BRIGHT + ("DOWN") + Fore.RESET,
+                    ping,
+                    ping_sum,
                 ]
             )
         elif prefix in bgp_peer_states:
@@ -110,10 +145,11 @@ def bgp_check(task):
                     neigh_as,
                     prefix,
                     up_down,
-                    Fore.YELLOW + ("WAITING") + Fore.RESET,
+                    Fore.YELLOW + Style.BRIGHT +  ("WAITING") + Fore.RESET,
+                    ping,
+                    ping_sum,
                 ]
             )
-
 
 def main() -> None:
     """Will execute the bgp_check"""
@@ -122,7 +158,8 @@ def main() -> None:
     # int_res = nr.run(task=interface_check)
     # print_result(bgp_res)
     # print_result(int_res)
-    print(tabulate(table, headers, tablefmt="rst", colalign="center"))
+    print(tabulate(table, headers, tablefmt="rst", colalign="right"))
+
 
 
 if __name__ == "__main__":
